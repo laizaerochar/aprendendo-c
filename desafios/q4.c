28eab982cf9f4a47cd8308b0cc8e487879aafa1f
laizaer <laizaerocha@gmail.com>
2026-02-24 17:47:58 -0300

    o mesmo exercicio, mas agora usando o while

---
+++ desafios/q4.c
@@ -0,0 +1,10 @@
+// o mesmp de q3.c mas agora usando o while 
+
+#include <stdio.h>
+int main() {
+    int numero = 1;
+    while(numero <= 100) {
+        printf("%d ", numero);
+        numero ++;
+    }
+}
