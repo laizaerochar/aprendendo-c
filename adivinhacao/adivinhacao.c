b2d8cd55ad886ed5165705a46b724a33cb0c1841
laizaer <laizaerocha@gmail.com>
2026-02-24 17:36:56 -0300

    cap estudando condicionais e loops

--- adivinhacao/adivinhacao.c
+++ adivinhacao/adivinhacao.c
@@ -1,4 +1,5 @@
 #include <stdio.h> // preciso incluir a biblioteca p escrever na tela
+
 int main() { // a função/o método principal é a main(), a partir dela que o programa comecar a ser executado.
 // printando uma mensagem de boas vindas
     printf("*****************************************\n");
@@ -7,11 +8,39 @@ int main() { // a função/o método principal é a main(), a partir dela que o 
     
     int numerosecreto = 42; //declarei uma var
     
+    
     int chute;
+    int tentativas = 1;
     
-    printf("Qual é o seu chute? ");
-    scanf("%d", &chute); //scanf lê do teclado
-    printf("Seu chute foi %d\n", chute);
+    while(1) {
     
-    //printf("O número secreto era %d e seu chute foi %d\n", numerosecreto, chute);
+        printf("Tentativa %d\n", tentativas); 
+        printf("Qual é o seu chute? ");
+        scanf("%d", &chute); //scanf lê do teclado
+        if (chute < 0) {
+            printf("Você não pode chutar número negativo\n");
+            
+            continue;
+        }
+        
+        int acertou = chute == numerosecreto;
+        int maior = chute > numerosecreto;
+        
+        if(acertou) {
+            printf("Parabéns! Você acertou!\n");
+            
+            break;
+        }
+         
+        else if(maior) {
+            printf("Seu chute foi maior que o número secreto\n");
+        }
+        
+        else {
+            printf("Seu chute foi menor que o número secreto\n");
+        }
+        tentativas++; // == tentativas = tentativas + 1;
+    }
+    printf("Fim de jogo!\n");
+    printf("Você acertou em %d", tentativas);
 }
