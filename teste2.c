9be30a069b58f7596a28f1c593bb73b27822db4a
laizaer <laizaerocha@gmail.com>
2026-02-25 13:06:25 -0300

    srand(parametro);

--- teste2.c
+++ teste2.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main() {
+    int segundos = time(0);
+    srand(segundos);
+    
     int n1 = rand();
     int n2 = rand();
     
