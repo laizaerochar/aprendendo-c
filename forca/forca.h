78a218ac41560d4231d31f695af4e5b1b340216b
laizaer <laizaerocha@gmail.com>
2026-03-25 09:45:44 -0300

    manipulacao de entrada e saida em c

---
+++ forca/forca.h
@@ -0,0 +1,10 @@
+// lista de funções que aparecerao no arquivo, o headerfile
+
+int enforcou();
+void abertura();
+void chuta();
+int jachutou(char letra);
+int ganhou();
+void desenhaforca();
+void escolhepalavra();
+void adicionapalavra();
