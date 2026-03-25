1898d4d9485c6953fcffb0532e0ced1cd12a4e17
laizaer <laizaerocha@gmail.com>
2026-03-25 02:30:47 -0300

    q15

---
+++ desafios/q15.c
@@ -0,0 +1,15 @@
+// Escreva uma função soma que recebe um ponteiro de inteiro num e mais dois inteiros a e b. A função deve calcular a soma de a+b em num.
+
+#include <stdio.h>
+ 
+ void soma(int* num, int a, int b) {
+     *num = a + b;
+ }
+ int main() {
+     int resultado;
+     
+     soma(&resultado, 5, 3); 
+     printf("resultado %d\n", resultado);
+     
+     return 0;
+ }
